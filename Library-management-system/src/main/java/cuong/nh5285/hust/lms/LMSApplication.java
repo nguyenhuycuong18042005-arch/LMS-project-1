2c995fffd5b8a49df109b9c969f2084c462f572f
HuyCuong <nguyenhuycuong18042005@gmail.com>
2025-11-29 01:02:56 +0700

    Initial commit

---
+++ Library-management-system/src/main/java/cuong/nh5285/hust/lms/LMSApplication.java
@@ -0,0 +1,13 @@
+package cuong.nh5285.hust.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LMSApplication.class, args);
+	}
+
+}
---
+++ Library-management-system/src/test/java/cuong/nh5285/hust/lms/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cuong.nh5285.hust.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
